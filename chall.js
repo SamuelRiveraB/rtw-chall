264215709790ab07590b62f7c65ff670e32e0f01
Samuel <samuelrivba@gmail.com>
2024-04-04 15:44:03 -0500

    Add chall overview

---
+++ chall.js
@@ -0,0 +1,12 @@
+// Technical Challenge Overview:
+
+// Your task is to initiate the development of a Facebook Group Search Tool. The core functionality of this tool will be to identify and list community or town Facebook groups that meet the following criteria. It should be able to take in town name and find all the other towns with an X mile radius. Then report back the following:
+
+// - Located within a X mile radius of X town.
+// - For example all groups with in 20 miles radius of Franklin, Massachusetts.
+// - The group type should be community or town groups where town members join to learn more about their community.
+// - Each group must have over 1,000 members.
+// - The groups should be private.
+// - excluding any business or buy/sell groups.
+// - Here is a link of a bunch of good groups in Massachusetts which you can use to understand what a community group looks like:
+// https://docs.google.com/spreadsheets/d/1kLJ84waTCy4mPdMviJZLP3H2C0Q1oPbDCfyCQhkwg5Y/edit?usp=sharing
